4f44c2d568caf6de7bda976ea377b38a2933dd4a
Natalie Anderson <natalieg.anderson@mail.utoronto.ca>
2025-02-10 11:30:04 -0500

    add comments

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kZXI5MjQiLCJhIjoiY201b2RweHNhMGxjazJscTI0cm92MDNuOCJ9.DUSIWV2_-BR0a9LOqhn15w';
 
+//creating map
 const createMap = (zoomValue) => {
 	const map = new mapboxgl.Map({
-		container: 'map', // container ID
+		container: 'map', 
 		style: 'mapbox://styles/ander924/cm6sdtrs2004t01qshcd17pgy', // style URL
-		center: [-123.126, 49.249], // starting position [lng, lat]
+		center: [-123.126, 49.249], // starting view [lng, lat]
 		zoom: zoomValue, // starting zoom
 		minZoom: zoomValue //can't zoom out
 	});
 	
 	map.on('load', () => {
-		// Load Public Art Layer
+		// Load Public Art GeoJSON into github
 		map.addSource('public-art', {
 			type: 'geojson',
 			data: 'https://raw.githubusercontent.com/NAnder924/GGR472_Lab2/refs/heads/main/public-art.geojson'
 		});
-	
+	//details regarding markers for public art
 		map.addLayer({
 			id: 'public-art-layer',
-			type: 'circle',
-			source: 'public-art',
+			type: 'circle', //type of marker
+			source: 'public-art', //using the source for public art added above
 			paint: {
-				'circle-radius': 3,
-				'circle-color': '#B33951', 
-				'circle-stroke-width': 0.5,
-				'circle-stroke-color': '#691E06'
+				'circle-radius': 3, //circle radius size
+				'circle-color': '#B33951',  //colour of circle
+				'circle-stroke-width': 0.5, //border of circle width
+				'circle-stroke-color': '#691E06' //border of circle colour
 			}
 		});
 	
+		// Load Community Gardens and Food Trees GeoJSON into github
 		map.addSource('community-gardens-and-food-trees', {
 			type: 'geojson',
 			data: 'https://raw.githubusercontent.com/NAnder924/GGR472_Lab2/refs/heads/main/community-gardens-and-food-trees.geojson'
 		});
-	
+	//details regarding markers for community gardens and food trees
 		map.addLayer({
 			id: 'community-gardens-and-food-trees', 
-			type: 'circle',
-			source: 'community-gardens-and-food-trees',
+			type: 'circle', //type of marker
+			source: 'community-gardens-and-food-trees', //using the source for community gardens and foodtrees added above
 			paint: {
-				'circle-radius': 3,
-				'circle-color': '#284B63',
-				'circle-stroke-width': 1,
-				'circle-stroke-color': '#2F3E46'
+				'circle-radius': 3, //circle radius size
+				'circle-color': '#284B63', //colour of circle
+				'circle-stroke-width': 1, //border of circle width
+				'circle-stroke-color': '#2F3E46' //border of circle colour
 			}
 		})
 	});
 }
 
 createMap(11)
+// creating legend popout and map characteristics
 
+// making button change from show legend to hide legend depending if opened or not
 const renameButton = () => {
 	const button = document.getElementById("button");
 	console.log(button.innerHTML)
@@ -57,14 +61,14 @@ const renameButton = () => {
 
 document.getElementById('button').addEventListener('click', () => {
 	const mapDiv = document.getElementById('map');
-
-	 if (mapDiv.style.width === '70vw') {
-		mapDiv.style.width='100vw'
-		mapDiv.style.height='100vh'
+//if map has this view than create map11, otherwise create map10.5
+	 if (mapDiv.style.width === '70vw') {//map view width 70%
+		mapDiv.style.width='100vw' //map view width 100%
+		mapDiv.style.height='100vh' //map view height 100%
 		createMap(11)
 	} else {
-		mapDiv.style.width='70vw'
-		mapDiv.style.height='100vh'
+		mapDiv.style.width='70vw' //map view width 70%
+		mapDiv.style.height='100vh' //map view height 100%
 		createMap(10.5)
 	}
 })
